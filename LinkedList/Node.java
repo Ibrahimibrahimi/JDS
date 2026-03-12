93ee93dc0c37eeef53e97e3869e1d41734964b99
Ibrahimibrahimi <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-03-12 12:31:13 +0000

    explain what is a <T> generator

--- LinkedList/Node.java
+++ LinkedList/Node.java
@@ -1,17 +1,29 @@
 public class Node<T> {
+    /*
+     * The '<T>' is called a generator ,
+     * that means no matter the data type u want
+     * to make this node (integer, string...)
+     * u don't have to define all constructors for
+     * each type , just make : Node mynode = new Node<String>Node()
+     * or the type u want
+     */
     private T data;
     private Node next;
-    public Node(T data){
+
+    public Node(T data) {
         this.data = data;
         this.next = null;
     }
-    public void addNext(Node node){
+
+    public void addNext(Node node) {
         this.next = node;
     }
-    public T getData(){
+
+    public T getData() {
         return data;
     }
-    public Node getNext(){
+
+    public Node getNext() {
         return this.next;
     }
 }
