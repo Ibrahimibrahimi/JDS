f6557c8e384f51f7c21b12f756d6da74810b25b7
Ibrahimibrahimi <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-03-12 12:46:34 +0000

    another example for addNodes(...nodes)

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -5,7 +5,14 @@ public class Main {
         test.addData(50);
         test.addData("ibrahim");
         test.addData(12.5);
-        test.addNode(new Node(12));;
+
         System.out.println(test.toString());
+
+        // test for circular
+        LinkedList l = new LinkedList();
+        l.addNodes(
+                new Node(1),
+                new Node(55));
+        System.out.println(l.toString());
     }
 }
