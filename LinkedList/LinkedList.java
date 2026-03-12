99e947c560df02a710e9b1155fbf847de9b54c9e
Ibrahimibrahimi <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-03-12 12:03:07 +0000

    Constructor for linked list

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,5 +1,18 @@
 
-
 public class LinkedList {
+    private Node HEAD , TAIL;
+    private int SIZE;
+    public LinkedList(){
+        this.HEAD = null; // if no head by default is null
+        this.TAIL = null;
+        this.SIZE = 0;
+    }
     
+    public LinkedList(Node DefaultHead){
+        this.HEAD = DefaultHead; // if no head by default is null
+        this.TAIL = null;
+        this.SIZE = 1;
+    }
+    
+
 }
